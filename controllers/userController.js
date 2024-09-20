39e4fb8fbfa224af459e981d04bacc272399d7e4
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-20 20:27:05 +0800

    move to controller

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+// Import the database queries module
+const db = require('../db/queries')
+
+// Function to handle getting all usernames
+
+async function getAllUsernames(req, res) {
+    try {
+        const usernames = await db.getAllUserNames()
+        console.log("Usernames: ", usernames);
+        res.send("Usernames: " + usernames.map(user => user.username).join(", "))
+    } catch (error) {
+        console.error('Error getting usernames:', error);
+        res.status(500).send('Failed to retrieve usernames');
+    }
+}
+
+// Function to handle inserting a new username
+async function addUsername(req, res) {
+    try {
+        const { username } = req.body;
+        await db.insertUsername(username)
+        console.log("usernames will be logged here - wip")
+        res.send(`Saved username: ${username}`)
+    } catch (error) {
+        console.error('Error saving username:', error);
+        res.status(500).send('Failed to save username');
+    }
+}
+
+module.exports = {
+    getAllUsernames,
+    addUsername
+};
--- index.js
+++ index.js
@@ -3,26 +3,18 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 const path = require('path')
-const db = require("./db/queries")
+const userController = require('./controllers/userController')
 
 // Middleware to parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', async (req, res) => {
-    const usernames = await db.getAllUserNames()
-    console.log("Usernames: ", usernames);
-    res.send("Usernames: " + usernames.map(user => user.username).join(", "))
-})
+app.get('/', userController.getAllUsernames)
 
 app.get('/new', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'form.html'))
 })
 
-app.post('/new', async (req, res) => {
-    const { username } = req.body;
-    await db.insertUsername(username)
-    console.log("usernames will be logged here - wip")
-})
+app.post('/new', userController.addUsername)
 
 app.listen(PORT, function (err) {
     if (err) console.log(err);
